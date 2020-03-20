ec65f6a6be43426af8fa9393d003e8404304edfe
aylabbs <35504864+aylabbs@users.noreply.github.com>
2020-03-20 15:53:23 -0400

    check audio available in loop

--- index.js
+++ index.js
@@ -7,29 +7,32 @@ const ytdl = require('ytdl-core');
 
 
 
-app.get('/watch', async (req, res) => {
-  if (!req.query.v) return res.end('Invalid link');
-  const link = `https://www.youtube.com/?v=${req.query.v}`;
-  const info = await ytdl.getInfo(link)
-  console.log(info)
-  let html = ''
-  
-  res.attachment('video.mp4')
-  ytdl(link).pipe(res)
+app.get('/download', async (req, res) => {
+  try {
+    if (!req.query.v) return res.end('Invalid link');
+    const link = `https://www.youtube.com/?v=${req.query.v}`;
+    res.attachment(`${req.query.name}.mp4`)
+    ytdl(link, { quality: req.query.quality }).pipe(res)  
+  } catch (e) {
+    res.end('Error occurred. Check link and try again')
+  }
 })
-app.get('/info', async (req, res) => {
+app.get('/watch', async (req, res) => {
   try {
     if (!req.query.v) return res.end('Invalid link');
     const link = `https://www.youtube.com/?v=${req.query.v}`;
     const info = await ytdl.getBasicInfo(link)
-    console.log(info)
-    let html = ''
-    info.formats.forEach(format => html = format.mimeType.includes('mp4') ? html + format.qualityLabel + "<br />" : html)
-    // res.attachment('video.mp4')
-    // ytdl(link).pipe(res)
-    // res.end(JSON.stringify(info))
+    let html = '<HTML>'
+    info.formats
+    .sort((first, second) => first.height > second.height )
+    .forEach(format => html = 
+      format.audioChannels && format.qualityLabel && format.mimeType.includes('mp4') ?
+        `${html}<a
+          href="/download?v=${req.query.v}&quality=${format.itag}&name=${info.title}"
+          >${format.qualityLabel} - ${format.quality}</a><br />` :
+        html)
+    html += '</HTML>'
     res.end(html)
-  
   } catch (e) {
     res.end('Error occurred. Check link and try again')
   }
