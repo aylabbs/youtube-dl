0579a83233a0d86e5e612abef25deda16941e0ca
aylabbs <35504864+aylabbs@users.noreply.github.com>
2020-03-20 14:29:52 -0400

    implement info

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const ytdl = require('ytdl-core');
 
 
 
-app.get('/', (req, res) => {
-  console.log(req.query)
+app.get('/watch', async (req, res) => {
   if (!req.query.v) return res.end('Invalid link');
   const link = `https://www.youtube.com/?v=${req.query.v}`;
-  console.log(link)
-
+  const info = await ytdl.getInfo(link)
+  console.log(info)
   res.attachment('video.mp4')
   ytdl(link).pipe(res)
+})
+app.get('/info', async (req, res) => {
+  if (!req.query.v) return res.end('Invalid link');
+  const link = `https://www.youtube.com/?v=${req.query.v}`;
+  const info = await ytdl.getInfo(link)
+  console.log(info)
+  // res.attachment('video.mp4')
+  // ytdl(link).pipe(res)
+  res.end(JSON.stringify(info))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
