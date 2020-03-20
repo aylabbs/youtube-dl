e39ec96ed95bc153d784efb7fec86b17fff38726
aylabbs <35504864+aylabbs@users.noreply.github.com>
2020-03-20 14:56:16 -0400

    install packages add gitignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 const ytdl = require('ytdl-core');
 
 
@@ -12,17 +12,27 @@ app.get('/watch', async (req, res) => {
   const link = `https://www.youtube.com/?v=${req.query.v}`;
   const info = await ytdl.getInfo(link)
   console.log(info)
+  let html = ''
+  
   res.attachment('video.mp4')
   ytdl(link).pipe(res)
 })
 app.get('/info', async (req, res) => {
-  if (!req.query.v) return res.end('Invalid link');
-  const link = `https://www.youtube.com/?v=${req.query.v}`;
-  const info = await ytdl.getBasicInfo(link)
-  console.log(info)
-  // res.attachment('video.mp4')
-  // ytdl(link).pipe(res)
-  res.end(JSON.stringify(info))
+  try {
+    if (!req.query.v) return res.end('Invalid link');
+    const link = `https://www.youtube.com/?v=${req.query.v}`;
+    const info = await ytdl.getBasicInfo(link)
+    console.log(info)
+    let html = ''
+    info.formats.forEach(format => html = format.mimeType.includes('mp4') ? html + format.qualityLabel + "<br />" : html)
+    // res.attachment('video.mp4')
+    // ytdl(link).pipe(res)
+    // res.end(JSON.stringify(info))
+    res.end(html)
+  
+  } catch (e) {
+    res.end('Error occurred. Check link and try again')
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
