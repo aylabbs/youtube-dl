8803bdc8281ac631f334373451a7744ae20dd152
Abraham Labkovsky <aylabbs@gmail.com>
2020-10-22 12:03:16 -0400

    add audio splicer

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9897;
 const ytdl = require('ytdl-core');
+const audioSplicer = require('./splicer');
 
 
 
@@ -11,8 +12,14 @@ app.get('/download', async (req, res) => {
   try {
     if (!req.query.v) return res.end('Invalid link');
     const link = `https://www.youtube.com/?v=${req.query.v}`;
-    res.attachment(`${req.query.name}.mp4`)
-    ytdl(link, { quality: req.query.quality }).pipe(res)  
+    console.log(req.query)
+    if (req.query.hasAudio === 'true') {
+      res.attachment(`${req.query.name}.mp4`)
+      ytdl(link, { quality: req.query.quality }).pipe(res)        
+    } else {
+      res.attachment(`${req.query.name}.mkv`)
+      audioSplicer(req, res, link)
+    }
   } catch (e) {
     res.end('Error occurred. Check link and try again')
   }
@@ -22,15 +29,17 @@ app.get('/watch', async (req, res) => {
     if (!req.query.v) return res.end('Invalid link');
     const link = `https://www.youtube.com/?v=${req.query.v}`;
     const info = await ytdl.getBasicInfo(link)
+    const { formats } = info
+    const mp4Formats = formats.filter(el => el.mimeType.split(';')[0] == 'video/mp4')
+    const audio = formats.filter(el => el.mimeType.split(';')[0] == 'audio/mp4')
+    // console.log(info)
     let html = '<HTML>'
-    info.formats
-    .sort((first, second) => first.height > second.height )
-    .forEach(format => html = 
-      format.audioChannels && format.qualityLabel && format.mimeType.includes('mp4') ?
-        `${html}<a
-          href="/download?v=${req.query.v}&quality=${format.itag}&name=${info.title}"
-          >${format.qualityLabel} - ${format.quality}</a><br />` :
-        html)
+    mp4Formats
+      .sort((first, second) => first.height > second.height )
+      .forEach(format => html += 
+        `<a href="/download?v=${req.query.v}&hasAudio=${!!format.audioChannels}&quality=${format.itag}&name=${info.title}"
+          >${format.qualityLabel} - ${format.quality} ${format.audioChannels ? ' - with audio' : ''}</a><br />`
+      )
     html += '</HTML>'
     res.end(html)
   } catch (e) {
---
+++ splicer.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const cp = require('child_process');
+const readline = require('readline');
+// External modules
+const ytdl = require('ytdl-core')
+const ffmpeg = require('ffmpeg-static');
+// Global constants
+const ref = 'https://www.youtube.com/watch?v=aR-KAldshAE';
+
+
+const audioSplicer = (req, res, link) => {
+
+    /**
+   * Reencode audio & video without creating files first
+   *
+   * Requirements: ffmpeg, ether via a manual installation or via ffmpeg-static
+   */
+
+  // Buildin with nodejs
+  const tracker = {
+    start: Date.now(),
+    audio: { downloaded: 0, total: Infinity },
+    video: { downloaded: 0, total: Infinity },
+    merged: { frame: 0, speed: '0x', fps: 0 },
+  };
+
+  // Get audio and video stream going
+  const audio = ytdl(link, { filter: 'audioonly', quality: 'highestaudio' })
+    .on('progress', (_, downloaded, total) => {
+      tracker.audio = { downloaded, total };
+    });
+  const video = ytdl(link, { filter: 'videoonly', quality: req.query.quality })
+    .on('progress', (_, downloaded, total) => {
+      tracker.video = { downloaded, total };
+    });
+
+  // Get the progress bar going
+  const progressbar = setInterval(() => {
+    readline.cursorTo(process.stdout, 0);
+    const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+    process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+    process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+    process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+    process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+    process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+    readline.moveCursor(process.stdout, 0, -3);
+  }, 1000);
+
+  // Start the ffmpeg child process
+  const ffmpegProcess = cp.spawn(ffmpeg, [
+    // Remove ffmpeg's console spamming
+    '-loglevel', '0', '-hide_banner',
+    // Redirect/enable progress messages
+    '-progress', 'pipe:3',
+    // 3 second audio offset
+    // '-itsoffset', '3.0', '-i', 'pipe:4',
+    '-i', 'pipe:5',
+    // Rescale the video
+    '-vf', 'scale=320:240',
+    // Choose some fancy codes
+    '-c:v', 'libx265', '-x265-params', 'log-level=0',
+    '-c:a', 'flac',
+    // Define output container
+    '-f', 'matroska', 'pipe:6',
+  ], {
+    windowsHide: true,
+    stdio: [
+      /* Standard: stdin, stdout, stderr */
+      'inherit', 'inherit', 'inherit',
+      /* Custom: pipe:3, pipe:4, pipe:5, pipe:6 */
+      'pipe', 'pipe', 'pipe', 'pipe',
+    ],
+  });
+  ffmpegProcess.on('close', () => {
+    process.stdout.write('\n\n\n\n');
+    clearInterval(progressbar);
+    console.log('done');
+  });
+
+  // Link streams
+  // FFmpeg creates the transformer streams and we just have to insert / read data
+  ffmpegProcess.stdio[3].on('data', chunk => {
+    // Parse the param=value list returned by ffmpeg
+    const lines = chunk.toString().trim().split('\n');
+    const args = {};
+    for (const l of lines) {
+      const [key, value] = l.trim().split('=');
+      args[key] = value;
+    }
+    tracker.merged = args;
+  });
+  audio.pipe(ffmpegProcess.stdio[4]);
+  video.pipe(ffmpegProcess.stdio[5]);
+  ffmpegProcess.stdio[6].pipe(res);
+}
+module.exports = audioSplicer
